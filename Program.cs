f7f83811429bf625aabcf45a1da8c8ea0c3331f1
ACER <ACER@LAPTOP-1SKMPNSL>
2024-04-17 01:27:57 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            danhSachHoaDon a = new danhSachHoaDon();
+            a.docFile("C:\\Users\\ACER\\Desktop\\Ôn\\2033223943_TranHoangQuan\\hoaDon.xml");
+            a.xuatDS();
+            Console.ReadLine();
+        }
+    }
+}
